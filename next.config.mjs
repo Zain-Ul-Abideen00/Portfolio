f6a27ac163f15bfe3c1ae8032ad46805a5de7594
Zain Ul Abideen <djzain33@gmail.comm>
2024-10-04 22:01:49 +0500

    Ignore Build Error

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,42 @@
 const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['cdn.sanity.io'],
+    },
+    env: {
+        NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+        NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+        NEXT_PUBLIC_SANITY_API_READ_TOKEN: process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN,
+        NEXT_PUBLIC_SANITY_API_WRITE_TOKEN: process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN,
+    },
+    experimental: {
+        appDir: true,
+    },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    },
+    headers: async () => {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
